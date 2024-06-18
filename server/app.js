1167aac9d8d461869e4d98d4c573753f641a48ec
Alexwilliam112 <alexwilliam112@gmail.com>
2024-06-18 22:50:05 +0700

    Added firebase config and class

--- server/app.js
+++ server/app.js
@@ -5,14 +5,16 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const http = require('http')
+const bodyParser = require('body-parser')
 const initializeSocket = require('./middleware/socket')
 const router = require('./routers/index')
 
 const app = express()
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(bodyParser.json())
 
 app.use(router)
 initializeSocket(server)
