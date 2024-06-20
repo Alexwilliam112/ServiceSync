6d8ad0582326e87bb6d75815524696d520d4bc9d
b-amadeo <bryansetio25@gmail.com>
2024-06-20 18:46:35 +0700

    Added Light And Dark Mode UI (Not Functional Yet)

--- client/src/layouts/BaseLayout.jsx
+++ client/src/layouts/BaseLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import { useContext } from "react";
+import { themeContext } from "../context/themeContext";
 
 export default function BaseLayout() {
+    const { currentTheme, theme } = useContext(themeContext);
+
     return (
         <>
-        <div className=" h-svh bg-slate-200">
+        <div className=" h-svh bg-slate-200" data-theme={theme[currentTheme].dataTheme}>
             <Navbar />
             <Outlet />
         </div>
