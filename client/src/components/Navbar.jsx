b9f36e6a97ec40c59813e637e6bffcd101c4b50e
b-amadeo <bryansetio25@gmail.com>
2024-06-20 10:24:17 +0700

    Added CreateRoom UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Toastify from "toastify-js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate();
 
   function handleLogout() {
@@ -28,17 +33,84 @@ export default function Navbar() {
     }
   }
 
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
+
   return (
     <>
       <div className="container rounded-lg shadow-lg">
-        <div className="flex items-center justify-between border-b-2 bg-white px-5 py-5 w-screen">
+        <div className="flex w-screen items-center justify-between border-b-2 bg-white px-5 py-5">
+          <button
+            onClick={toggleModal}
+            className="block rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-500 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            type="button"
+          >
+            Add Room
+          </button>
           <div className="text-2xl font-semibold">ServiceSync</div>
-          <button onClick={handleLogout}>Logout</button>
+          <button
+            onClick={handleLogout}
+            className="rounded bg-red-600 px-4 py-2 text-white hover:bg-red-700 focus:outline-none"
+          >
+            Logout
+          </button>
           <div className="flex h-12 w-12 items-center justify-center rounded-full bg-purple-500 p-2 font-semibold text-white">
             {localStorage.username.charAt(0).toUpperCase()}
           </div>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div
+          id="authentication-modal"
+          tabIndex="-1"
+          aria-hidden="true"
+          className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto bg-gray-800 bg-opacity-50"
+        >
+          <div className="relative w-full max-w-md p-4">
+            <div className="relative rounded-lg bg-white shadow">
+              <div className="flex items-center justify-between rounded-t border-b p-4">
+                <h3 className="text-xl font-semibold text-gray-900">
+                  Add Room
+                </h3>
+                <FontAwesomeIcon
+                  icon={faX}
+                  type="button"
+                  onClick={toggleModal}
+                  className="ms-auto inline-flex items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900"
+                  style={{ fontSize: "1rem" }}
+                />
+              </div>
+              <div className="p-4">
+                <form className="space-y-4">
+                  <div>
+                    <label
+                      htmlFor="room-name"
+                      className="mb-2 block text-sm font-medium text-gray-900"
+                    >
+                      Room Name
+                    </label>
+                    <input
+                      type="text"
+                      name="room-name"
+                      id="room-name"
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+                      placeholder="Enter room name"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  >
+                    Create Room
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
