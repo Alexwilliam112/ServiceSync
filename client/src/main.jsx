6a985a7be86dc5a7cd781b58be7fe1037574ba38
Alexwilliam112 <alexwilliam112@gmail.com>
2024-06-21 02:07:08 +0700

    Fixed import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "toastify-js/src/toastify.css";
+import App from "./App.jsx";
+import ThemeProvider from "./context/ThemeContext.jsx";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import ThemeProvider from "./context/themeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
