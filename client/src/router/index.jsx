2f778bb1bd012ff7c749368488012ddb2ee03b3d
Alexwilliam112 <alexwilliam112@gmail.com>
2024-06-21 09:35:32 +0700

    Fixed import

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -5,7 +5,7 @@ import Toastify from 'toastify-js';
 import BaseLayout from "../layouts/BaseLayout.jsx";
 import ChatPage from "../views/ChatPage.jsx";
 import LoginPage from "../views/LoginPage.jsx";
-import TemporaryPage from "../views/temp.jsx";
+import TemporaryPage from "../views/Temp.jsx";
 
 const url = "https://server.cozyb.me"
 const socket = io("https://server.cozyb.me", {
